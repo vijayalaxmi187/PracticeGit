e93ac3e352f410819fc77b1b6999452990fa0fc1
vijayalakshmi <vijayalakshmi@DESKTOP-7MIKM5U>
2022-03-24 13:13:31 +0530

    First commit github practice project

---
+++ GitPracticeProject/src/test/java/gitHubPractice/LeadCreation.java
@@ -0,0 +1,17 @@
+package gitHubPractice;
+
+import org.testng.annotations.Test;
+
+public class LeadCreation {
+
+		@Test
+		public void lead() {
+			System.out.println("User1 organization created ");
+			
+		}
+		
+
+	}
+
+
+
---
+++ GitPracticeProject/src/test/java/gitHubPractice/OrgCreation.java
@@ -0,0 +1,13 @@
+package gitHubPractice;
+
+import org.testng.annotations.Test;
+
+public class OrgCreation {
+	@Test
+	public void create() {
+		System.out.println("User1 organization created ");
+		
+	}
+	
+
+}
